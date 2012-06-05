b7f3c54c782b241d625995a9aa876f22892bea39
apb <bunce.andy@gmail.com>
2012-06-05 22:44:34 +0100

    switchy to jQuery

--- snorql/snorql.js
+++ snorql/snorql.js
@@ -14,7 +14,7 @@ function Snorql() {
     // modify these to your likeing
     this._poweredByLink = 'http://jena.apache.org/documentation/serving_data/';
     this._poweredByLabel = 'Fuseki';
-    this._enableNamedGraphs = false;
+    this._enableNamedGraphs = true;
 
     this._browserBase = null;
     this._namespaces = {};
@@ -158,8 +158,8 @@ function Snorql() {
     }
 
     this._displayPoweredBy = function() {
-        $('poweredby').href = this._poweredByLink;
-        $('poweredby').update(this._poweredByLabel);
+        $('#poweredby').attr('href',  this._poweredByLink);
+        $('#poweredby').text(this._poweredByLabel);
     }
 
     this.setNamespaces = function(namespaces) {
@@ -177,9 +177,9 @@ function Snorql() {
 
     this._updateGraph = function(uri, effect) {
         if (!this._enableNamedGraphs) {
-            $('default-graph-section').hide();
-            $('named-graph-section').hide();
-            $('browse-named-graphs-link').hide();
+            $('#default-graph-section').hide();
+            $('#named-graph-section').hide();
+            $('#browse-named-graphs-link').hide();
             return;
         }
         var changed = (uri != this._graph);
@@ -188,30 +188,30 @@ function Snorql() {
         el.disabled = (this._graph == null);
         el.value = this._graph;
         if (this._graph == null) {
-            var show = 'default-graph-section';
-            var hide = 'named-graph-section';
-            $$('a.graph-link').each(function(link) {
-                match = link.href.match(/^(.*)[&?]graph=/);
-                if (match) link.href = match[1];
+            var show = '#default-graph-section';
+            var hide = '#named-graph-section';
+            $('a.graph-link').each(function(ind) {
+                match = $(this).attr('href').match(/^(.*)[&?]graph=/);
+                if (match) $(this).attr('href',match[1]);
             });
         } else {
-            var show = 'named-graph-section';
-            var hide = 'default-graph-section';
-            $('selected-named-graph').update(this._graph);
+            var show = '#named-graph-section';
+            var hide = '#default-graph-section';
+            $('#selected-named-graph').text(this._graph);
             var uri = this._graph;
-            $$('a.graph-link').each(function(link) {
-                match = link.href.match(/^(.*)[&?]graph=/);
-                if (!match) link.href = link.href + '&graph=' + uri;
+            $('a.graph-link').each(function(ind) {
+                match = $(this).attr("href").match(/^(.*)[&?]graph=/);
+                if (!match) $(this).attr("href", $(this).attr("href") + '&graph=' + uri);
             });
         }
         $(hide).hide();
         $(show).show();
         if (effect && changed) {
-            new Effect.Highlight(show,
-                {startcolor: '#ffff00', endcolor: '#ccccff', resotrecolor: '#ccccff'});
+        	$(show).effect("highlight", {}, 3000);
+         //   new Effect.Highlight(show,   {startcolor: '#ffff00', endcolor: '#ccccff', resotrecolor: '#ccccff'});
         }
-        $('graph-uri').disabled = (this._graph == null);
-        $('graph-uri').value = this._graph;
+        $('#graph-uri').disabled = (this._graph == null);
+        $('#graph-uri').text( this._graph);
     }
 
     this.updateOutputMode = function() {
